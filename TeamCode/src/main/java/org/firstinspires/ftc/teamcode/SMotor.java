e478ea0fbe7b8d9a9d72a819eb75653c19edfd7d
FTCUser <techturtles10179@gmail.com>
2018-11-03 13:58:31 -0500

    Self-calibrating SmartMotors fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SMotor.java
@@ -7,8 +7,11 @@ import com.qualcomm.robotcore.util.Range;
 public class SMotor {
     private DcMotor motor;
 
+    private int startingPos;
+    private int currentSpeed;
+    private int stage = 0;
     private int threshhold = 3;
-    private int slowLength  = 50;
+    private int slowLength = 50;
     private int goToPosition = 0;
     private double positiveCalibration = 0.5;
     private double negativeCalibration = -0.5;
@@ -19,10 +22,11 @@ public class SMotor {
     private static final DcMotor.RunMode STOP_AND_RESET_ENCODER = DcMotor.RunMode.STOP_AND_RESET_ENCODER;
 
     public boolean isAtPosition = true;
+    public boolean alreadyCalibrated = false;
 
-    DcMotor.RunMode mode = RUN_WITHOUT_ENCODER;
+    DcMotor.RunMode mode = DcMotor.RunMode.RUN_USING_ENCODER;
 
-    SMotor (DcMotor tempMotor) {
+    SMotor(DcMotor tempMotor) {
         motor = tempMotor;
     }
 
@@ -40,26 +44,42 @@ public class SMotor {
         goToPosition = pos;
     }
 
-    public void calibrate() {
-        int startingPos = motor.getCurrentPosition();
-        int currentSpeed = 0;
-        while (motor.getCurrentPosition() == startingPos && currentSpeed < 1) {
-            currentSpeed += 0.05;
-            motor.setPower(currentSpeed);
+    private void calibrate() {
+        switch (stage) {
+            case 0:
+                motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                startingPos = motor.getCurrentPosition();
+                currentSpeed = 0;
+                stage++;
+                break;
+            case 1:
+                if (motor.getCurrentPosition() == startingPos && currentSpeed < 1) {
+                    stage++;
+                }
+                currentSpeed += 0.05;
+                motor.setPower(Range.clip(currentSpeed, -1, 1));
+                break;
+            case 2:
+                motor.setPower(0);
+                positiveCalibration = currentSpeed;
+                startingPos = motor.getCurrentPosition();
+                currentSpeed = 0;
+                stage++;
+                break;
+            case 3:
+                if (motor.getCurrentPosition() == startingPos && currentSpeed > -1) {
+                    stage++;
+                }
+                currentSpeed -= 0.05;
+                motor.setPower(Range.clip(currentSpeed, -1, 1));
+                break;
+            case 4:
+                stage++;
+                alreadyCalibrated = true;
+                negativeCalibration = currentSpeed;
+                motor.setPower(0);
+                break;
         }
-
-        motor.setPower(0);
-        positiveCalibration = currentSpeed;
-
-        startingPos = motor.getCurrentPosition();
-        currentSpeed = 0;
-        while (motor.getCurrentPosition() == startingPos && currentSpeed > -1) {
-            currentSpeed -= 0.05;
-            motor.setPower(currentSpeed);
-        }
-
-        negativeCalibration = currentSpeed;
-        motor.setPower(0);
     }
 
     public void setMode(DcMotor.RunMode tempMode) {
@@ -71,43 +91,47 @@ public class SMotor {
     }
 
     public void update() {
-        switch (mode) {
-            case RUN_TO_POSITION:
-                if (motor.getMode() != DcMotor.RunMode.RUN_USING_ENCODER) {
-                    motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                }
-
-                isAtPosition = Math.abs(motor.getCurrentPosition() - goToPosition) <= threshhold;
-
-                if (isAtPosition) {
-                    if (Math.abs(positiveCalibration) > Math.abs(negativeCalibration)) {
-                        motor.setPower(positiveCalibration - 0.01);
-                    } else {
-                        motor.setPower(negativeCalibration + 0.01);
+        if (alreadyCalibrated) {
+            switch (mode) {
+                case RUN_TO_POSITION:
+                    if (motor.getMode() != DcMotor.RunMode.RUN_USING_ENCODER) {
+                        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
                     }
-                } else {
-                    if (motor.getCurrentPosition() < goToPosition) {
-                        motor.setPower(Range.clip((Math.abs(motor.getCurrentPosition()-goToPosition)+positiveCalibration)/slowLength, -1, 1));
+
+                    isAtPosition = Math.abs(motor.getCurrentPosition() - goToPosition) <= threshhold;
+
+                    if (isAtPosition) {
+                        if (Math.abs(positiveCalibration) > Math.abs(negativeCalibration)) {
+                            motor.setPower(positiveCalibration - 0.05);
+                        } else {
+                            motor.setPower(negativeCalibration + 0.05);
+                        }
                     } else {
-                        motor.setPower(Range.clip(-(Math.abs(motor.getCurrentPosition()-goToPosition)+negativeCalibration)/slowLength, -1, 1));
+                        if (motor.getCurrentPosition() < goToPosition) {
+                            motor.setPower(Range.clip((Math.abs(motor.getCurrentPosition() - goToPosition) + positiveCalibration) / slowLength, -1, 1));
+                        } else {
+                            motor.setPower(Range.clip(-(Math.abs(motor.getCurrentPosition() - goToPosition) + negativeCalibration) / slowLength, -1, 1));
+                        }
                     }
-                }
-                break;
-            case STOP_AND_RESET_ENCODER:
-                if (motor.getMode() != DcMotor.RunMode.STOP_AND_RESET_ENCODER) {
-                    motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                }
-                break;
-            case RUN_USING_ENCODER:
-                if (motor.getMode() != DcMotor.RunMode.RUN_USING_ENCODER) {
-                    motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                }
-                break;
-            case RUN_WITHOUT_ENCODER:
-                if (motor.getMode() != DcMotor.RunMode.RUN_WITHOUT_ENCODER) {
-                    motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-                }
-                break;
+                    break;
+                case STOP_AND_RESET_ENCODER:
+                    if (motor.getMode() != DcMotor.RunMode.STOP_AND_RESET_ENCODER) {
+                        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                    }
+                    break;
+                case RUN_USING_ENCODER:
+                    if (motor.getMode() != DcMotor.RunMode.RUN_USING_ENCODER) {
+                        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                    }
+                    break;
+                case RUN_WITHOUT_ENCODER:
+                    if (motor.getMode() != DcMotor.RunMode.RUN_WITHOUT_ENCODER) {
+                        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    }
+                    break;
+            }
+        } else {
+            calibrate();
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -10,7 +11,7 @@ import org.firstinspires.ftc.teamcode.WheelController;
 
 @TeleOp(name = "Tech Turtles TeleOp")
 
-public class Teleop extends OpMode {
+public class Teleop extends LinearOpMode {
     int tiltEncoderMax = 0;
     int tiltEncoderMin = 0;
     int tiltLiftEncoder = 0;
@@ -24,7 +25,7 @@ public class Teleop extends OpMode {
     WheelController wheelController;
 
     @Override
-    public void init() {
+    public void runOpMode() {
         // Normally we would initialise the wheel motors here, but our motor controller class does that for us
         wheelController = new WheelController(hardwareMap);
         // Initialize everything else
@@ -33,75 +34,70 @@ public class Teleop extends OpMode {
         tiltLift = new SMotor(hardwareMap.dcMotor.get("tiltLift"));
         tiltLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         //tiltLift.calibrate();
-    }
 
-    @Override
-    public void start() {
-        super.start();
-        tiltLift.calibrate();
-    }
+        waitForStart();
 
-    @Override
-    public void loop() {
-        // The distance from the joystick's position to the joystick's origin
-        double r = Math.hypot(-gamepad1.left_stick_x, gamepad1.left_stick_y);
+        while (opModeIsActive()) {
+            // The distance from the joystick's position to the joystick's origin
+            double r = Math.hypot(-gamepad1.left_stick_x, gamepad1.left_stick_y);
 
-        // The angle at which the will move the robot
-        double robotAngle = Math.atan2(gamepad1.left_stick_y, -gamepad1.left_stick_x) - Math.PI / 4;
+            // The angle at which the will move the robot
+            double robotAngle = Math.atan2(gamepad1.left_stick_y, -gamepad1.left_stick_x) - Math.PI / 4;
 
-        // Let's make the right joystick's x position a variable so it doesn't change as we are doing calculations
-        double rightX = -gamepad1.right_stick_x;
+            // Let's make the right joystick's x position a variable so it doesn't change as we are doing calculations
+            double rightX = -gamepad1.right_stick_x;
 
-        // Do the calculations for the wheel speeds
-        double v1 = r * Math.cos(robotAngle) + rightX;
-        double v2 = r * Math.sin(robotAngle) - rightX;
-        double v3 = r * Math.sin(robotAngle) + rightX;
-        double v4 = r * Math.cos(robotAngle) - rightX;
+            // Do the calculations for the wheel speeds
+            double v1 = r * Math.cos(robotAngle) + rightX;
+            double v2 = r * Math.sin(robotAngle) - rightX;
+            double v3 = r * Math.sin(robotAngle) + rightX;
+            double v4 = r * Math.cos(robotAngle) - rightX;
 
-        // Finally, take the math we did for the speed of the wheels and actually set the wheel's speed
-        wheelController.frontLeft.setPower(v1);
-        wheelController.frontRight.setPower(v2);
-        wheelController.backLeft.setPower(v3);
-        wheelController.backRight.setPower(v4);
+            // Finally, take the math we did for the speed of the wheels and actually set the wheel's speed
+            wheelController.frontLeft.setPower(v1);
+            wheelController.frontRight.setPower(v2);
+            wheelController.backLeft.setPower(v3);
+            wheelController.backRight.setPower(v4);
 
 
-        if (gamepad1.left_bumper) {
-            tiltEncoderMin -= 10;
+            if (gamepad1.left_bumper) {
+                tiltEncoderMin -= 10;
+            }
+            if (gamepad1.right_bumper) {
+                tiltEncoderMin += 10;
+            }
+            if (gamepad1.left_trigger > 0.5) {
+                tiltEncoderMax -= 10;
+            }
+            if (gamepad1.right_trigger > 0.5) {
+                tiltEncoderMax += 10;
+            }
+
+            if (gamepad1.a) {
+                tiltLiftEncoder += tiltLiftSpeed;
+            } else if (gamepad1.b) {
+                tiltLiftEncoder -= tiltLiftSpeed;
+            }
+
+            if (tiltLiftEncoder > tiltEncoderMax) {
+                tiltLiftEncoder = tiltEncoderMax;
+            } else if (tiltLiftEncoder < tiltEncoderMin) {
+                tiltLiftEncoder = tiltEncoderMin;
+            }
+
+            if (gamepad1.x) {
+                tiltLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                tiltLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            }
+
+            tiltLift.setTargetPosition(tiltLiftEncoder);
+
+            telemetry.addData("Current Encoder", tiltLift.getCurrentPosition());
+            telemetry.addData("Encoder", tiltLiftEncoder);
+            telemetry.addData("Encoder Min", tiltEncoderMin);
+            telemetry.addData("Encoder Max", tiltEncoderMax);
+
+            tiltLift.update();
         }
-        if (gamepad1.right_bumper) {
-            tiltEncoderMin += 10;
-        }
-        if (gamepad1.left_trigger > 0.5) {
-            tiltEncoderMax -= 10;
-        }
-        if (gamepad1.right_trigger > 0.5) {
-            tiltEncoderMax += 10;
-        }
-
-        if (gamepad1.a) {
-            tiltLiftEncoder += tiltLiftSpeed;
-        } else if (gamepad1.b) {
-            tiltLiftEncoder -= tiltLiftSpeed;
-        }
-
-        if (tiltLiftEncoder > tiltEncoderMax) {
-            tiltLiftEncoder = tiltEncoderMax;
-        } else if (tiltLiftEncoder < tiltEncoderMin) {
-            tiltLiftEncoder = tiltEncoderMin;
-        }
-
-        if (gamepad1.x) {
-            tiltLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            tiltLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        }
-
-        tiltLift.setTargetPosition(tiltLiftEncoder);
-
-        telemetry.addData("Current Encoder", tiltLift.getCurrentPosition());
-        telemetry.addData("Encoder", tiltLiftEncoder);
-        telemetry.addData("Encoder Min", tiltEncoderMin);
-        telemetry.addData("Encoder Max", tiltEncoderMax);
-
-        tiltLift.update();
     }
 }
